75486a56f04ef11ab72fca553f05d7ac415306f6
mtrejo17 <marcostrejoreyes@gmail.com>
2022-02-26 11:04:37 -0600

    se genera servidor de prueba

--- lib/server.ts
+++ lib/server.ts
@@ -1,2 +1,15 @@
 
 
+import * as express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hola desde mi servidor" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
